f5335b689efa75bb0dc5aaae1967feb45f1d7b45
Ritesh2803 <riteshshah238@gmail.com>
2023-12-14 21:43:36 +0530

    gk

---
+++ Hindi-job/All-Quiz/Reasoning/timer.js
@@ -0,0 +1,20 @@
+function startTimer(){
+    let timeLimit=60;
+    remainingTime.innerHTML=timeLimit;
+    remainingTime.classList.remove("less-time");
+    interval=setInterval(()=>{
+      timeLimit--;
+        if(timeLimit < 10){
+            timeLimit="0"+timeLimit;
+            
+            }
+            if(timeLimit < 6){
+                remainingTime.classList.add("less-time");  
+            }
+            remainingTime.innerHTML=timeLimit;
+            if(timeLimit == 0){
+            clearInterval(interval);
+            timeIsUp();
+    }
+    },1000)
+}

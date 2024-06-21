3a54bcde9f14dbce236bc7126eadb848f9864f4f
Ritesh2803 <riteshshah238@gmail.com>
2024-06-21 15:15:28 +0530

    Create time-count.js

---
+++ exel-seet-javascript/time-count.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+var countDownDate = new Date("august 24, 2024 14:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = "इस फॉर्म को भरने के लिए "+ days +" दिन " + hours + " घंटा "
+  + minutes + " मिनट  " + seconds + " सेकंड बचें है";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "इस फॉर्म को भरने की तारीख निकल चुकी है";
+  }
+}, 1000);

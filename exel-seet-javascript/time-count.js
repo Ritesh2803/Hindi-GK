7157db4f418bb04b8d298046cc9d5f4a476de0a1
Ritesh2803 <riteshshah238@gmail.com>
2024-06-21 15:35:13 +0530

    Update time-count.js

--- exel-seet-javascript/time-count.js
+++ exel-seet-javascript/time-count.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("august 24, 2024 14:59:59").getTime();
+
 
 // Update the count down every 1 second
 var x = setInterval(function() {

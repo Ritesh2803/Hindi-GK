af50cf7fc93609e32a37f3bb9573bdb202c0b028
Ritesh2803 <riteshshah238@gmail.com>
2024-08-26 03:21:50 +0530

    Update search.php

--- Hindi-job/search.php
+++ Hindi-job/search.php
@@ -14,8 +14,8 @@
 </div>
 
 <?php   
-$base="Techno Smarter"; // Change base here 
-$search_URL="http://www.google.com/search?q="; // Google Search Query URL  
+$base=""; // Change base here 
+$search_URL="https://jstrieb.github.io/link-lock/#"; // Google Search Query URL  
 if(isset($_POST['keys_submit'])){ 
 echo $keywords=$_POST['keywords'];
  header("location: ".$search_URL.$keywords.' '.$base );
